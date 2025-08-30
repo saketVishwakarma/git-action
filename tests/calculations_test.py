61ece8add36345c99c73b478d42f73cc22421fea
SAKET KUMAR VISHWAKARMA <svsaket123@gmail.com>
2025-08-30 10:26:07 +0000

    updated test file

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -61,10 +61,10 @@ def test_get_nth_fibonacci_one():
 def test_get_nth_fibonacci_ten():
     """Test with n=10."""
     # Arrange
-     n = 10
+    n = 10
 
     # Act
     result = get_nth_fibonacci(n)
 
     # Assert
-    assert result == 89
+    assert result == 55
